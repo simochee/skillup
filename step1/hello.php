91fa10b972543279cc62cc5ed050846c9cfd31f6
Ryoya Tamura <simochee@gmail.com>
2017-10-12 15:46:35 +0900

    連想配列やった

--- step1/hello.php
+++ step1/hello.php
@@ -8,12 +8,16 @@
 </head>
 <body>
   <?php
-    function double_print($text) {
-      print("$text$text");
-    }
+    //$hash["one"]が"いち", $hash["two"]が"に", $hash["three"]が"さん"となる連想配列を作成
+    $hash = array("one" => "いち", "two" => "に", "three" => "さん");
+    $hash["four"] = "し";
+    print_r($hash);
 
-    double_print('a');
-    double_print('bc');
+    echo '<br>';
+
+    //$hashの各要素を取り出して処理
+    foreach($hash as $key => $val)
+      print("$key is $val. ");
   ?>
 </body>
 </html>
